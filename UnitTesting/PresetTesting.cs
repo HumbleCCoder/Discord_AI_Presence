0274723e7b15045b91ca924d8a7f8ad38df5a250
HumbleCCoder <153657473+HumbleCCoder@users.noreply.github.com>
2023-12-15 23:48:49 -0500

    Cleanup

--- UnitTesting/PresetTesting.cs
+++ UnitTesting/PresetTesting.cs
@@ -1,25 +1,22 @@
-﻿using Discord_AI_Presence.Text_WebUI;
-using Discord_AI_Presence.Text_WebUI.Presets;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discord_AI_Presence.Text_WebUI.Presets;
 
 namespace UnitTesting
 {
-    internal class PresetTesting : TextUI_Presets
+    internal class PresetTestingHelper : TextUI_Presets
     {
-        [Test]
-        public void TestPresetLocation()
-        {
-            Assert.That(CurPreset.do_sample, Is.True);
-        }
-
         public override string PresetsLocation(string filename = "")
         {
             string solutionDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..");
             return Path.Combine(solutionDirectory, "Text_WebUI\\Presets\\Preset_Files", filename).Replace("UnitTesting", string.Empty);
         }
+    }
+    internal class TEST_TextUI_Presets
+    { 
+        [Test]
+        public void TestPresetLocation()
+        {
+            PresetTestingHelper preset = new();
+            Assert.That(preset.CurPreset.do_sample, Is.True);
+        }        
     }
 }
