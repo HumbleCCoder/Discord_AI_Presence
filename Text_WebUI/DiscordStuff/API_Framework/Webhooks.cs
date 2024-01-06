ac293efdb33cfbb479396a8ec171528e9d4b7ec9
Chad <153657473+HumbleCCoder@users.noreply.github.com>
2024-01-06 11:54:59 -0500

    Files

---
+++ Text_WebUI/DiscordStuff/API_Framework/Webhooks.cs
@@ -0,0 +1,34 @@
+﻿using Discord.WebSocket;
+using System.Collections.Specialized;
+using Discord_AI_Presence.Text_WebUI.ProfileScripts;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Discord_AI_Presence.Text_WebUI.DiscordStuff.API_Framework
+{
+    public class Webhooks
+    {
+        public async Task SendWebhookMessage(SocketGuild sg, ProfileData characterProfile, Settings serverSettings, string neuroMsg, ulong channelID)
+        {
+            var webhooks = await sg.GetWebhooksAsync();
+            if (webhooks == null || webhooks.Count == 0) { return; }
+            var url = !string.IsNullOrEmpty(serverSettings.Webhooks) ? serverSettings.Webhooks :
+                $"https://discord.com/api/webhooks/{webhooks.First().Id}/{webhooks.First().Token}";
+            await webhooks.First().ModifyAsync(x => x.ChannelId = channelID);
+            var webhookPayload = new
+            {
+                username = characterProfile.NickOrName(),
+                avatar_url = characterProfile.AvatarUrl,
+                content = neuroMsg,
+            };
+            await PostToWebhook(url, webhookPayload);
+        }
+        private static async Task PostToWebhook(string webhookUrl, object data)
+        {
+            using var httpClient = new HttpClient();
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(webhookUrl, content);
+        }
+    }
+}
---
+++ UnitTesting/NeuroNetTesting.cs
@@ -0,0 +1,31 @@
+﻿using Discord_AI_Presence.Text_WebUI;
+using Discord_AI_Presence.Text_WebUI.DiscordStuff;
+using Discord_AI_Presence.Text_WebUI.MemoryManagement;
+using Discord_AI_Presence.Text_WebUI.TextWebUI;
+using Discord_AI_Presence.Text_WebUI.ProfileScripts;
+using Discord_AI_Presence.Text_WebUI.Presets;
+namespace Discord_AI_Presence.UnitTesting
+{
+    internal class NeuroNetTesting
+    {
+        private const string username = "Jeff",
+                message = "Hello how are you?";
+        private const ulong
+            serverId = 5,
+            channelId = 9,
+            chatStarterUserID = 55,
+            msgID = 99;
+        private readonly ProfileData characterProfile = TextUI_Base.GetInstance().Cards.First().Value;
+        [Test]
+        public async Task SubmitToNeuroNet()
+        {
+            var server = new TextUI_Servers(serverId);
+            server.StartChat(new Chats(channelId, characterProfile, TextUI_Presets.PresetEnum.Mirostat, username, Text_WebUI.Instructions.Scenario.ScenarioPresets.Chatbot, chatStarterUserID));
+            server.AIChats.First().AddMessage(username, message, chatStarterUserID, msgID);
+            var result = await Connection.PostMessage(characterProfile, server, server.AIChats.First(), server.AIChats.First().Participants());
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.Not.Empty);
+            Console.WriteLine(result);
+        }
+    }
+}
